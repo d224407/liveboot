2bcce68192ad50cea558233da3d4d87cbe113dac
d224407 <nguynlanh63@gmaio.com>
2026-07-27 14:33:11 +0700

    update

--- template/customize.sh
+++ template/customize.sh
@@ -85,9 +85,7 @@ ui_print "- Setting file permissions"
 
 set_perm "$MODPATH/system/bin/liveboot" 0 0 0755
 set_perm "$MODPATH/config" 0 0 0644
-set_perm "$MODPATH/service.sh" 0 0 0755
 set_perm "$MODPATH/post-fs-data.sh" 0 0 0755
-set_perm "$MODPATH/action.sh" 0 0 0755
 set_perm "$MODPATH/uninstall.sh" 0 0 0755
 
 ui_print ""
